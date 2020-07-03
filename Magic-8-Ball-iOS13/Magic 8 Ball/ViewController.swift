f59e499bdf778d9ac2d15c3ab89a5e8001df740a
Amogh Chaubey <chaubeyamogh@gmail.com>
2020-07-03 14:37:34 -0400

    I made a magic 8 ball app!!!

--- Magic-8-Ball-iOS13/Magic 8 Ball/ViewController.swift
+++ Magic-8-Ball-iOS13/Magic 8 Ball/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    let ballArray = [#imageLiteral(resourceName: "ball3"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
 
-
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var askButtonPressed: UIButton!
+    
+    
+    @IBAction func askButtonPressed(_ sender: Any) {
+        imageView.image = ballArray[Int.random(in: 0...4)]
+    }
+    
 }
 
